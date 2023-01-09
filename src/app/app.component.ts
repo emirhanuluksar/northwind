2b59a09e20b6f6bf40a509d6d5a6e787b76e52ef
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-01-09 20:42:16 +0300

    IIS ADDING TRANSACTION

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // sen app.component.html in datasını yönetecek olan componentsin
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'northwind';
+export class AppComponent { // Angularda her şey class nesne güdümlü bir yapıya sahiptir.
+  title: string = 'northwind';
+  user: string = 'Emirhan Uluksar';
+
 }
+// Htmlin datasını yönettiğimiz yer componenttir.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// Module --> temel anlamda birbiriyle ilişkili componentleri directivleri ve farklı bileşenleri topladığımız yer
+
+// CORS --> Origin request
